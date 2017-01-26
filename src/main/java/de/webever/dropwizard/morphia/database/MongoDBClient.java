8f33933a6f582dbfb0fa65951074455402838e3e
Richard Naeve <r.naeve@webever.de>
2017-01-26 15:23:36 +0100

    Added new query short hands

--- src/main/java/de/webever/dropwizard/morphia/database/MongoDBClient.java
+++ src/main/java/de/webever/dropwizard/morphia/database/MongoDBClient.java
@@ -20,126 +20,140 @@ import io.dropwizard.lifecycle.Managed;
 
 public class MongoDBClient implements Managed {
 
-    final Morphia morphia = new Morphia();
+	final Morphia morphia = new Morphia();
 
-    final MongoDBConfiguration configuration;
+	final MongoDBConfiguration configuration;
 
-    final Datastore datastore;
+	final Datastore datastore;
 
-    final MongoClient client;
+	final MongoClient client;
 
-    private HashMap<Class<? extends Model>, List<Consumer<? extends Model>>> saveHooks = new HashMap<>();
+	private HashMap<Class<? extends Model>, List<Consumer<? extends Model>>> saveHooks = new HashMap<>();
 
-    public MongoDBClient(MongoDBConfiguration configuration, String modelPackage) {
-	this.configuration = configuration;
-	morphia.mapPackage(modelPackage, true);
-	client = new MongoClient(configuration.host, configuration.port);
-	datastore = morphia.createDatastore(client, configuration.dataStore);
-	datastore.ensureIndexes();
-    }
-
-    @Override
-    public void start() throws Exception {
-    }
-
-    @Override
-    public void stop() throws Exception {
-	client.close();
-    }
-
-    /**
-     * Returns the raw mongodb client. Use with caution.
-     * 
-     * @return the raw client
-     */
-    public MongoClient getClient() {
-	return client;
-    }
-
-    public <T extends Model> void addSaveHook(Class<T> clazz, Consumer<T> consumer) {
-	List<Consumer<? extends Model>> consumers = saveHooks.get(clazz);
-	if (consumers == null) {
-	    consumers = new ArrayList<>();
+	public MongoDBClient(MongoDBConfiguration configuration, String modelPackage) {
+		this.configuration = configuration;
+		morphia.mapPackage(modelPackage, true);
+		client = new MongoClient(configuration.host, configuration.port);
+		datastore = morphia.createDatastore(client, configuration.dataStore);
+		datastore.ensureIndexes();
 	}
-	consumers.add(consumer);
-	saveHooks.put(clazz, consumers);
-    }
 
-    @SuppressWarnings("unchecked")
-    public <T extends Model> T save(T model) {
-	if (model.getId() == null) {
-	    model.setId(UUID.randomUUID().toString());
+	@Override
+	public void start() throws Exception {
 	}
-	Key<T> key = datastore.save(model);
-	T savedModel = findById(key.getId().toString(), (Class<T>) model.getClass());
-	List<Consumer<? extends Model>> consumers = saveHooks.get(model.getClass());
-	if (consumers != null) {
-	    for (Consumer<? extends Model> c : consumers) {
-		Consumer<T> consumer = (Consumer<T>) c;
-		consumer.accept(savedModel);
-	    }
+
+	@Override
+	public void stop() throws Exception {
+		client.close();
 	}
-	return savedModel;
-    }
 
-    public <T extends Model> T findById(String id, Class<T> clazz) {
-	return findByField("id", id, clazz);
-    }
-
-    public <T extends Model> T findByField(String field, Object value, Class<T> clazz) {
-	return datastore.createQuery(clazz).field(field).equal(value).get();
-    }
-
-    public boolean healthy() {
-	try {
-	    client.getDatabase(configuration.dataStore).listCollections();
-	    return true;
-	} catch (Exception e) {
-	    return false;
+	/**
+	 * Returns the raw mongodb client. Use with caution.
+	 * 
+	 * @return the raw client
+	 */
+	public MongoClient getClient() {
+		return client;
 	}
-    }
 
-    public <T extends Model> List<T> findAll(Class<T> clazz) {
-	return datastore.createQuery(clazz).asList();
-    }
+	public <T extends Model> void addSaveHook(Class<T> clazz, Consumer<T> consumer) {
+		List<Consumer<? extends Model>> consumers = saveHooks.get(clazz);
+		if (consumers == null) {
+			consumers = new ArrayList<>();
+		}
+		consumers.add(consumer);
+		saveHooks.put(clazz, consumers);
+	}
 
-    public <T extends Model> Query<T> createQuery(Class<T> clazz) {
-	return datastore.createQuery(clazz);
-    }
-
-    public <T extends Model> void delete(T model) {
-	datastore.delete(model);
-    }
-    
-    public <T extends Model> void delete(Query<T> query) {
-	datastore.delete(query);
-    }
-
-    public <T extends Model> void updateField(String fieldName, Object value, T model) {
 	@SuppressWarnings("unchecked")
-	UpdateOperations<T> operation = (UpdateOperations<T>) datastore.createUpdateOperations(model.getClass());
-	operation.set(fieldName, value);
-	datastore.update(model, operation);
-    }
-
-    public <T extends Model> Query<? extends Model> refModelIn(Query<? extends Model> query, String field, Class<T> clazz, List<T> models) {
-	List<String> ids = new ArrayList<>();
-	for (T t : models) {
-	    ids.add(t.getId());
+	public <T extends Model> T save(T model) {
+		if (model.getId() == null) {
+			model.setId(UUID.randomUUID().toString());
+		}
+		Key<T> key = datastore.save(model);
+		T savedModel = findById(key.getId().toString(), (Class<T>) model.getClass());
+		List<Consumer<? extends Model>> consumers = saveHooks.get(model.getClass());
+		if (consumers != null) {
+			for (Consumer<? extends Model> c : consumers) {
+				Consumer<T> consumer = (Consumer<T>) c;
+				consumer.accept(savedModel);
+			}
+		}
+		return savedModel;
 	}
-	return refIn(query, field, clazz, ids);
-    }
 
-    public <T extends Model> Query<? extends Model> refIn(Query<? extends Model> query, String field, Class<T> clazz, List<String> ids) {
-	List<Key<T>> list = new ArrayList<>();
-	for (String id : ids) {
-	    list.add(new Key<>(clazz, clazz.getSimpleName(), id));
+	public <T extends Model> T findById(String id, Class<T> clazz) {
+		return findByField("id", id, clazz);
 	}
-	return query.field(field).in(list);
-    }
 
-    public <T extends Model> Query<? extends Model> refEqual(Query<? extends Model> query, String field, Class<T> clazz, String id) {
-	return query.field(field).equal(new Key<>(clazz, clazz.getSimpleName(), id));
-    }
+	public <T extends Model> T findByField(String field, Object value, Class<T> clazz) {
+		return datastore.createQuery(clazz).field(field).equal(value).get();
+	}
+
+	public <T extends Model> List<T> findAllByField(String field, Object value, Class<T> clazz) {
+		return datastore.createQuery(clazz).field(field).equal(value).asList();
+	}
+
+	public boolean healthy() {
+		try {
+			client.getDatabase(configuration.dataStore).listCollections();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public <T extends Model> List<T> findAll(Class<T> clazz) {
+		return datastore.createQuery(clazz).asList();
+	}
+
+	public <T extends Model> Query<T> createQuery(Class<T> clazz) {
+		return datastore.createQuery(clazz);
+	}
+
+	public <T extends Model> void delete(T model) {
+		datastore.delete(model);
+	}
+
+	public <T extends Model> void delete(Query<T> query) {
+		datastore.delete(query);
+	}
+
+	public <T extends Model> void updateField(String fieldName, Object value, T model) {
+		@SuppressWarnings("unchecked")
+		UpdateOperations<T> operation = (UpdateOperations<T>) datastore.createUpdateOperations(model.getClass());
+		operation.set(fieldName, value);
+		datastore.update(model, operation);
+	}
+
+	public <T extends Model> void addToField(String fieldName, Object value, T model) {
+		@SuppressWarnings("unchecked")
+		UpdateOperations<T> operation = (UpdateOperations<T>) datastore.createUpdateOperations(model.getClass());
+		operation.addToSet(fieldName, value);
+		datastore.update(model, operation);
+	}
+
+	public <T extends Model> Query<? extends Model> refModelIn(Query<? extends Model> query, String field,
+			Class<T> clazz, List<T> models) {
+		List<String> ids = new ArrayList<>();
+		for (T t : models) {
+			ids.add(t.getId());
+		}
+		return refIn(query, field, clazz, ids);
+	}
+
+	public <T extends Model> Query<? extends Model> refIn(Query<? extends Model> query, String field, Class<T> clazz,
+			List<String> ids) {
+		List<Key<T>> list = new ArrayList<>();
+		for (String id : ids) {
+			list.add(new Key<>(clazz, clazz.getSimpleName(), id));
+		}
+		return query.field(field).in(list);
+	}
+
+	public <T extends Model> Query<? extends Model> refEqual(Query<? extends Model> query, String field, Class<T> clazz,
+			String id) {
+		return query.field(field).equal(new Key<>(clazz, clazz.getSimpleName(), id));
+	}
 
 }
